6a2eea18b2ca33fbd786e9788c44591cc2bcbfdd
yougenanhai <2387082008@qq.com>
2020-05-05 20:35:06 +0800

    commit

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -28,7 +28,7 @@ public class CategoryController {
     /**
      * 列表
      */
-    @RequestMapping("/list/search")
+    @RequestMapping("/list")
     //@RequiresPermissions("pms:category:list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = categoryService.queryPage(params);
