18b74660bcf072e77b26b672142163bd919ddb83
yougenanhai <2387082008@qq.com>
2020-05-05 20:34:06 +0800

    commit

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
@@ -27,12 +28,22 @@ public class CategoryController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/search")
     //@RequiresPermissions("pms:category:list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = categoryService.queryPage(params);
 
         return R.ok().put("page", page);
+    }
+
+    /**
+     * 查出所有分类以及子分类，并以树形结构组装起来
+     */
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+
+        return R.ok().put("data", entities);
     }
 
 
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -10,7 +10,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -20,10 +23,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
                 new Query<CategoryEntity>().getPage(params),
-                new QueryWrapper<CategoryEntity>()
+                new QueryWrapper<>()
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1、查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //2、组装成父子的树形结构
+        //2.1）、找到所有的一级分类
+        return entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).peek((menu)-> menu.setChildren(getChildrens(menu,entities))
+        ).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))
+        ).collect(Collectors.toList());
+    }
+
+    private List<CategoryEntity> getChildrens(CategoryEntity root,List<CategoryEntity> all){
+        //递归查找所有菜单的子菜单
+        return all.stream().filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId())
+        ).peek(categoryEntity -> {
+            //1、找到子菜单
+            categoryEntity.setChildren(getChildrens(categoryEntity,all));
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))
+        ).collect(Collectors.toList());
+
     }
 
 }
